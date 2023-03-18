29a7d5ce83e24bf6c3145a84d47dd8b27f8739d3
Tayoga123 <kunztayoga.titananda.kusnadi@gmail.com>
2023-03-18 07:52:36 +0700

    Initial commit

---
+++ app/src/main/java/com/darker/ukl2/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.darker.ukl2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ScrollView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
